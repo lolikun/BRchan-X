31bef2169939b6a01c7e0c8122855e9c9493e3c4
lolikun <birixixi@gmail.com>
2012-09-29 17:02:25 -0300

    release 1.80b

--- README.md
+++ README.md
@@ -1,88 +1,21 @@
 BRchan-X
 ========
 
+DESCRIÇÃO
+
+ - Script para o brchan com diversas funcionalidades que melhoram a usabilidade
+
 CONTRIBUIÇÕES
 
- - Miofag - Adaptação do 4chan X updater para o kusaba
- - Lolikun - Novas implementações
-
+ - lolikun - Novas implementações
 
 TODO
 
  - Quotes em cascata
  - Diálogos movíveis
  - Deletar posts sem recarregar a página
+ 
+CRÉDITOS
 
-CHANGELOG
-
- 1.80b
- - Melhorias no estilo do quick reply
- - Adicionado evento que abre o quick reply ao apertar a tecla enter
-
- 1.80a
- - Trocado o estilo do quick reply
-
- 1.74
- - Corrigidos mais problemas no catálogo
-
- 1.73
- - Corrigido o problema no catálogo
- - Desativado o 'If-Modified-Since' por problemas de compatibilidade
-
- 1.72
- - Corrigido outro bug introduzido na versão 1.70
-
- 1.71
- - Corrigido um bug introduzido na versão 1.70 onde algumas coisas pararam de funcionar
-
- 1.70
- - Adicionado quick reply
- - Adicionado texto no catálogo
- - Adicionado o cabeçalho 'If-Modified-Since' para evitar carregamento desnecessário
- - Adicionado delay de 6 horas entre as verificações de atualização
-
- 1.60
- - Adicionada autalização automática
-
- 1.57
- - Contador de posts/imagens fica vermelho quando thread entra em autosage
-
- 1.56
- - Consertado o problema onde os links direcionavam para um post qualquer
-
- 1.55
- - Consertado o botão de 'Expandir todas as imagens' para funcionar com fit screen/gifs animadas
- - Adicionado contador do total de posts/imagens
-
- 1.54
- - Opção "esconder sage" agora fica salva
-
- 1.53
- - Adicionada compatibilidade com o Chrome
- - Trocado o valor subtraído no cálculo de tamanho máximo das imagens
-
- 1.52
- - Campo de senha permanente trocado de texto para password
-
- 1.51
- - Trocado o favicon de 404
-
- 1.50
- - Adicionado autonoko
- - Adicionada senha permanente
-
- 1.41
- - Correção do link do userscripts
-
- 1.40
- - Troca da função que faz o get da página (verificar se não quebrou alguma coisa)
- - Consertado o contador de respostas
- - Consertada a detecção de 404
- - Consertada a configuração de intervalo
- - Adicionado menu de opções
-
-
-
-Modificado a partir do 4chan X Updater (aeosynth)
-
-Algumas funcionalidades foram baseadas no 4chan X (Mayhem)
+ - aeosynth - Criação do 4chan X, no qual várias funcionalidades foram baseadas
+ - mayhem - Atual mantedor do 4chan X
--- brchanx.user.js
+++ brchanx.user.js
@@ -2,93 +2,9 @@
 // @name           BRchan X 
 // @namespace      Lolikun
 // @description    Faz update da thread. Agora com backlinks, gifs animadas, hide automático de posts com sage e expandir imagens ao clicar.
-// @version        1.80b
+// @version        1.81
 // @include        http://*.brchan.org/*
 // ==/UserScript==
-
-/* CONTRIBUIÇÕES
-*
-* - Miofag
-* - Lolikun
-*
-*
-* TODO
-*
-* - Quotes em cascata
-*
-*
-* CHANGELOG
-*
-* 1.80b
-* - Melhorias no estilo do quick reply
-* - Adicionado evento que abre o quick reply ao apertar a tecla enter
-*
-* 1.80a
-* - Trocado o estilo do quick reply
-*
-* 1.74
-* - Corrigidos mais problemas no catálogo
-*
-* 1.73
-* - Corrigido o problema no catálogo
-* - Desativado o 'If-Modified-Since' por problemas de compatibilidade
-*
-* 1.72
-* - Corrigido outro bug introduzido na versão 1.70
-*
-* 1.71
-* - Corrigido um bug introduzido na versão 1.70 onde algumas coisas pararam de funcionar
-*
-* 1.70
-* - Adicionado quick reply
-* - Adicionado texto no catálogo
-* - Adicionado o cabeçalho 'If-Modified-Since' para evitar carregamento desnecessário
-* - Adicionado delay de 6 horas entre as verificações de atualização
-*
-* 1.60
-* - Adicionada autalização automática
-*
-* 1.57
-* - Contador de posts/imagens fica vermelho quando thread entra em autosage
-*
-* 1.56
-* - Consertado o problema onde os links direcionavam para um post qualquer
-*
-* 1.55
-* - Consertado o botão de 'Expandir todas as imagens' para funcionar com fit screen/gifs animadas
-* - Adicionado contador do total de posts/imagens
-*
-* 1.54
-* - Opção "esconder sage" agora fica salva
-*
-* 1.53
-* - Adicionada compatibilidade com o Chrome
-* - Trocado o valor subtraído no cálculo de tamanho máximo das imagens
-*
-* 1.52
-* - Campo de senha permanente trocado de texto para password
-*
-* 1.51
-* - Trocado o favicon de 404
-*
-* 1.50
-* - Adicionado autonoko
-* - Adicionada senha permanente
-*
-* 1.41
-* - Correção do link do userscripts
-*
-* 1.40
-* - Troca da função que faz o get da página (verificar se não quebrou alguma coisa)
-* - Consertado o contador de respostas
-* - Consertada a detecção de 404
-* - Consertada a configuração de intervalo
-* - Adicionado menu de opções
-*
-*/
-
-// Modificado a partir do 4chan X Updater (aeosynth)
-// Algumas funcionalidades foram baseadas no 4chan X (Mayhem)
 
 // Copyright (c) 2010 James Campos
 
@@ -114,7 +30,7 @@
 
 (function() {
 
-var versaoatual = '1.80b';
+var versaoatual = '1.81';
 var linkvchecker = 'https://raw.github.com/lolikun/BRchan-X/master/stable/latest.js';
 var linkscript = 'https://raw.github.com/lolikun/BRchan-X/master/stable/brchanx.user.js';
 
@@ -538,7 +454,6 @@ dialogo.innerHTML = '\
 				<li>- Senha permanente</li>\
 			</ul>\
 			<ul>Contribuições:\
-				<li>- Miofag - Adaptação do 4chan X updater para o kusaba</li>\
 				<li>- Lolikun - Novas implementações</li>\
 			</ul>\
 			Caso encontre algum bug favor notificar na página do userscripts\
